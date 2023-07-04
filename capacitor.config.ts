9c833a991b597c94d9041a76911544499153fc7a
Stefan Agatonovic <s.agatonovic@d3eif.com>
2023-07-04 08:52:05 +0200

    icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,22 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'fridger',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+    },
+  },
 };
 
 export default config;
